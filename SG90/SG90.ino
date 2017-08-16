76bfd6d5cdd5cd95589ba39377d281169c701108
lokhinw <lokhinw@users.noreply.github.com>
2017-08-16 01:14:23 -0400

    Update SG90.ino

--- SG90/SG90.ino
+++ SG90/SG90.ino
@@ -11,12 +11,12 @@ void setup()
 
 void loop()
 {
-   servo.write(45);      // Turn 45 degrees
-   delay(1000);          // Wait 1 second
-   servo.write(90);      // Turn 90 degrees (center position)
-   delay(1000);          // Wait 1 second
-   servo.write(135);     // Turn 135 degrees
-   delay(1000);          // Wait 1 second
-   servo.write(90);      // Turn 90 degrees (center position)
-   delay(1000);          // Wait 1 second
+  servo.write(45);      // Turn 45 degrees
+  delay(1000);          // Wait 1 second
+  servo.write(90);      // Turn 90 degrees (center position)
+  delay(1000);          // Wait 1 second
+  servo.write(135);     // Turn 135 degrees
+  delay(1000);          // Wait 1 second
+  servo.write(90);      // Turn 90 degrees (center position)
+  delay(1000);          // Wait 1 second
 }
